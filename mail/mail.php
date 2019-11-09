34cfec7983cc0b5675693ce964a435bb7fdc7bfb
nikhilmudka <40689264+nikhilmudka@users.noreply.github.com>
2019-11-10 01:25:40 +0530

    Update mail.php

--- mail/mail.php
+++ mail/mail.php
@@ -16,9 +16,9 @@ $error = array();
 if(empty($error))
 { 
 $mail =
-mail("alankarrocks@mail.com", "$title", $message,
- "From: alankarrocks@mail.com\r\n"
-."Reply-To: alankarrocks@mail.com\r\n"
+mail("mudkanikhil@mail.com", "$title", $message,
+ "From: mudkanikhil@mail.com\r\n"
+."Reply-To: mudkanikhil@mail.com\r\n"
 ."Content-type:text/plain;charset=utf-8\r\n"
 ."X-Mailer: PHP/" . phpversion());
 
